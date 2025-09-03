873e3b5809e7a100ca2dfd240bc88b3ad2af310c
LEONARDO TAGLIAFERRO <202502321503@alunos.estacio.br>
2025-09-02 21:51:06 -0300

    Exercicio 1 de python

---
+++ ex1.py
@@ -0,0 +1,9 @@
+nome = (input("digite seu nome completo: "))
+idade = int(input("digite sua idade: "))
+sexo = input("digite seu sexo") 
+endereco = input ("digite seu endereco: ")
+telefone = input("digite seu telefone: ")
+
+
+print("seu nome e:", nome, "\n", "sua idade e", idade, "\n", "seu sexo e: ", sexo, "\n", "seu endereco e: ", endereco, "\n", "seu telefone e: ", telefone)
+
