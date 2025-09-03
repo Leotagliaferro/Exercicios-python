4a3fa3ee7eae83a91f87ddee56cd4fd5d1169888
LEONARDO TAGLIAFERRO <202502321503@alunos.estacio.br>
2025-09-02 22:05:24 -0300

    Exercicio 2 concluido

---
+++ ex2.py
@@ -0,0 +1,6 @@
+nota1 = input("digite sua primeira nota")
+nota2 = input("digite sua segunda nota")
+nota3 = input("Digite sua terceira nota")
+nota4 = input("digite sua quarta nota")
+
+print("suas notas foram ", nota1, "\n", nota2, "\n", nota3, "\n", nota4) 
