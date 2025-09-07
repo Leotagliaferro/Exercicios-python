400372ddad4a0cf421ea850d52ce47c8b0d43616
leotagliaferro <tagliaferro.leonardo@gmail.com>
2025-09-07 11:02:37 -0300

    primeira lista de python

---
+++ ex4.py
@@ -0,0 +1,5 @@
+nome = input("Digite seu nome: ")
+placa= input("digite a placa: ")
+cor = input("Digite a cor do carro: ")
+
+print("nome: " , nome, "\n", "placa: " , placa , "\n", "cor do carro: ", cor)
---
+++ ex6.py
@@ -0,0 +1,5 @@
+base = float(input("digite o valor da base: "))
+altura = float(input("digite o valor da altura: "))
+soma = base * altura
+
+print("A area do retangulo é de: ", soma)
